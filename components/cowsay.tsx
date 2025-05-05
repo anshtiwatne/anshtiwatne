24dd93b8bea8eb707310928f3d0ac11a469b1952
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2025-05-05 18:48:32 +0530

    shorten cow

--- components/cowsay.tsx
+++ components/cowsay.tsx
@@ -3,7 +3,7 @@ import Link from '@/components/link'
 export default function CowSay({ text, link }: { text: string; link: string }) {
 	return (
 		<pre
-			className="pt-4 text-left tracking-normal"
+			className="pt-4 text-left leading-tight tracking-normal"
 			style={{ fontFamily: 'monospace' }}
 		>
 			{` ${'_'.repeat(text.length + 2)}

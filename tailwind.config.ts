0e8e8e25a8d038563e96beefdd71b0d89014b6ff
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2025-08-24 13:24:17 +0530

    remove heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ export default {
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
 	],
 	darkMode: 'class',
 } satisfies Config
